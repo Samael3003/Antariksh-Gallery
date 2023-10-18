c6ffc97092a3ab757fb26f75908852a091df1f6a
Samael3003 <103866475+Samael3003@users.noreply.github.com>
2023-10-18 21:15:10 +0530

    Update Gallery.jsx

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -23,6 +23,26 @@ function Gallery() {
       imageUrl: 'https://github.com/Auriel3003/samael/assets/103866475/3cbbb90f-6fb0-4670-918d-c175b51a39c3',
       link: 'https://samael.vercel.app/posts/suresh-naik',
     },
+        {
+      title: 'Internal Talk',
+      imageUrl: 'https://github.com/Auriel3003/samael/assets/103866475/50e41c09-dfad-4546-8759-26f3b11ced6e',
+      link: 'https://samael.vercel.app/posts/internaltalk',
+    },
+        {
+      title: 'Chandrayaan-3 @Mumbai',
+      imageUrl: 'https://media.licdn.com/dms/image/D5612AQElsyAZLdlzbg/article-inline_image-shrink_1000_1488/0/1693041807201?e=1703116800&v=beta&t=fMaRnewOLof5v0GsGOJctoQD_6BalgXC1Dh9C6eyuUY',
+      link: 'https://samael.vercel.app/',
+    },
+    {
+      title: 'ASRT Workshop',
+      imageUrl: 'https://github.com/Auriel3003/samael/assets/116882665/4306e5ef-49ce-4643-829d-7fc2ebfe3ef3',
+      link: 'https://samael.vercel.app/',
+    },
+    {
+      title: 'Milkyway (AAkashganga)',
+      imageUrl: 'https://github.com/Auriel3003/samael/assets/116882665/4e287682-8fcd-4d5e-b629-a52880604be2',
+      link: 'https://samael.vercel.app/',
+    },
     // Add more images and links as needed
   ];
 
